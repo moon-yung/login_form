aed426c464ad1dc2f59e82fbb687a8739c0b804b
moon-yung <123131421+moon-yung@users.noreply.github.com>
2023-02-19 12:55:08 +0800

    Update loginSys.py

--- loginSys.py
+++ loginSys.py
@@ -44,7 +44,7 @@ def loginRights(username, password):
                     else:
                         new_pass = hashlib.sha256(new_password.encode('utf-8')).hexdigest()# encryption
                         update_password(username, password, new_pass)
-                        print(f"Password changed to {new_pass}!")
+                        print(f"Password changed!")
                         break
 
             elif choice == "5":
@@ -90,7 +90,7 @@ def main():
                         print(f"First Name: {first}")
                         print(f"Last Name : {last}")
                         print(f"Username  : {un}")
-                        print(f"Password  : {pw}")
+                        print(f"Password  : {pword}")
                         print(f"Date Registered: {current_date}")
                         break
 
